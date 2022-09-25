57b72a5026cea624621671a4cce51fb38ac6d129
Yelena Leri <yelenaleri@gmail.com>
2022-09-26 01:10:49 +0300

    3 ex

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  msg: document.querySelector('.feedback-form textarea'),
+};
+
+const FORM_KEY = 'feedback-form-state';
+
+const localData = {};
+
+populateForm();
+
+refs.form.addEventListener('input', throttle(inputForm, 500));
+refs.form.addEventListener('submit', submitForm);
+
+function inputForm(e) {
+  localData[e.target.name] = e.target.value;
+
+  localStorage.setItem(FORM_KEY, JSON.stringify(localData));
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+
+  const savedFormData = JSON.parse(localStorage.getItem(FORM_KEY));
+  console.log(savedFormData);
+  localStorage.removeItem(FORM_KEY);
+}
+
+function populateForm() {
+  const savedForm = JSON.parse(localStorage.getItem(FORM_KEY));
+
+  if (savedForm === null) {
+    return;
+  }
+
+  if (savedForm.email) {
+    refs.email.value = savedForm.email;
+    localData['email'] = refs.email.value;
+  }
+  if (savedForm.message) {
+    refs.msg.value = savedForm.message;
+    localData['message'] = refs.msg.value;
+  }
+}
