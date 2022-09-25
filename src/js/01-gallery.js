ea82f84de93146e5efd7177299623987ddca5dd9
Yelena Leri <yelenaleri@gmail.com>
2022-09-25 21:24:48 +0300

    1 ex

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const getGallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+getGallery.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
